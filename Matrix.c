3f713a74fb84ea537cf0d88fe4360207a1faca1f
rharshith2410 <68144219+rharshith2410@users.noreply.github.com>
2020-07-23 21:13:54 +0530

    Created on 23 July

---
+++ Matrix.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+  int matrix[3][3];
+  int i,j,sum=0;
+  
+  printf("Enter the elements of the matrix\n");
+  for(i=0;i<3;i++)
+  for(j=0;j<3;j++)
+  {
+    scanf("%d",&matrix[i][j]);
+  }
+ 
+  while(i==j)
+  {
+    sum=sum+matrix[i][j];
+    i++;
+    j++;
+  }
+  printf("\nThe sum of the diagonal elements is %d,sum);
+}
